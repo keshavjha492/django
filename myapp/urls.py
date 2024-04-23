199d6343efc187d2bc9f29bbaa55432465ea9e2a
keshavjha492 <keshavjha492@gmail.com>
2024-04-23 19:20:54 +0545

    portfolio

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home, portfolio
 
 urlpatterns = [
-    path("home/",home)
+    path("home/",home),
+    path("portfolio/",portfolio)
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -2,5 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
-    return HttpResponse("hello world this is response")
+    query_string = request.GET
+    name = query_string.get("name")
+    
+    return HttpResponse(f"hello world this is {name},")
+    
+    
+def portfolio(request):
+    return render(request,template_name="myapp/portfolio.html")
+
     
