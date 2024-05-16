40b341f4957be70c5c7783bfc944048dd1c18222
keshavjha492 <keshavjha492@gmail.com>
2024-05-16 19:22:09 +0545

    student update

--- classbased/forms.py
+++ classbased/forms.py
@@ -1,11 +1,17 @@
 from django import forms
-from crud.models import ClassRoom
+from crud.models import ClassRoom, Student
 
 class ClassRoomForm(forms.Form):
     name = forms.CharField(max_length=20, widget=forms.TextInput(attrs={'placeholder': 'class name'}))
-    
-    
+
 class ClassRoomModelForm(forms.ModelForm):
     class Meta:
         model = ClassRoom
-        fields = ['name']           #fields = '__all__'
+        fields = ["name", ]     #__all__ is used to add all fields
+
+class StudentModelForm(forms.ModelForm):
+    phone_number = forms.CharField(max_length = 14)
+    bio = forms.CharField(widget=forms.Textarea(attrs={'rows': 5}))
+    class Meta:
+        model = Student
+        fields = "__all__"
--- classbased/views.py
+++ classbased/views.py
@@ -1,104 +1,133 @@
-from typing import Any
+from django.forms import BaseModelForm
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
-from crud.models import ClassRoom
-from django.views.generic import TemplateView, ListView, CreateView, UpdateView, DeleteView
+from crud.models import ClassRoom, Student, StudentProfile
+from django.views.generic import TemplateView, ListView, CreateView, UpdateView, DeleteView, DetailView
 from django.contrib import messages
-from .forms import ClassRoomForm, ClassRoomModelForm
+from .forms import ClassRoomForm, ClassRoomModelForm, StudentModelForm
+
+
+
+# Create your views here.
 
 
 class HomeView(TemplateView):
     template_name = "classbased/home.html"
-    
-    
-    def get_context_data(self, **kwargs):
+
+    def get_context_data(self, **kwargs ):
         context = super().get_context_data(**kwargs)
-        context["name"] = "harry"
+        context["name"] = "Suraj"
+        print(context)
         return context
-    
+
     def get(self, request, *args, **kwargs):
         return super().get(request, *args, **kwargs)
-    
-    
-class ClassRoomView(ListView):
-    queryset= ClassRoom.objects.all()
-    template_name ="classbased/classroom.html"
 
-    def get_context_data(self,*args, **kwargs):
-        context = super().get_context_data(*args,**kwargs)
-        form = ClassRoomForm()
-        context["form"] = form
-        return context 
-    
-    def post(self, *args, **kwargs):
-        form = ClassRoomForm(self.request.POST)
-        if form.is_valid():
-            print(form.cleaned_data)
-            # name = form.cleaned_data["name"]
-            # ClassRoom.objects.create(name=name)
-            form.save()
-            messages.success(self.request, "classroom added successfully")
-            
-        else:
-            messages.error(self.request, "invalid form data")
-        return redirect("classbased:classroom")
-    
+
+# class ClassRoomView(ListView):
+#     queryset = ClassRoom.objects.all()
+#     template_name = "classbased/classroom.html"
+#     context_object_name = "classrooms"
+
+
+#     def get_context_data(self, *args, **kwargs):
+#         context = super().get_context_data(*args, **kwargs)
+#         # form = ClassRoomForm()
+#         form = ClassRoomModelForm()
+#         context["form"] = form
+        # return context
+
+    # def post(self, *args, **kwargs):
+    #     form = ClassRoomModelForm(self.request.POST)    # we are validating user input data
+    #     if form.is_valid():
+    #         print(form.cleaned_data)  # form.cleaned is a dict type
+    #         # name = form.cleaned_data["name"]
+    #         # ClassRoom.objects.create(name=name)
+    #         form.save()
+    #         messages.success(self.request, "Classroom added successfully")
+    #     else:
+    #         messages.error(self.request, "invalid form data")
+    #     return redirect("classbased:classroom")      
+
 class ClassroomListCreateView(CreateView):
-    queryset = ClassRoom.objects.all()
-    context_object_name="classrooms"
+    queryset = ClassRoom.objects.all()      #querysets are lazy in django
     template_name = "classbased/classroom.html"
+    context_object_name = "classrooms"
     form_class = ClassRoomModelForm
-    success_url  = reverse_lazy("classbased:classroom")
-    
+    success_url = reverse_lazy("classbased:classroom")
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['classrooms']= self.get_queryset
+        context["classrooms"] = self.get_queryset()
         return context
-    
-    
-    def post(self, *args, **kwargs):
+
+
+    def post(self, request, *args, **kwargs):
         self.object = None
         form = self.get_form()
         if form.is_valid():
-            messages.success(request,"classroom is added successfully")
+            messages.success(request, "Classroom added successfully")
             return self.form_valid(form)
         else:
-            messages.success(request,"Something went wrong")
+            messages.error(request, "Something went wrong !")
             return self.form_invalid(form)
-        
+
 class ClassroomUpdateView(UpdateView):
     queryset = ClassRoom.objects.all()
     template_name = "classbased/classroom_update.html"
     form_class = ClassRoomModelForm
     context_object_name = "classroom"
     success_url = reverse_lazy("classbased:classroom")
-       
-    
+
 class ClassroomDeleteView(DeleteView):
     queryset = ClassRoom.objects.all()
     template_name = "classbased/classroom_delete.html"
     success_url = reverse_lazy("classbased:classroom")
-    context_object_name = "classroom"
-    
-    def post(self, *args, **kwargs):
+
+    def post(self, request, *args, **kwargs):
         self.object = self.get_object()
         form = self.get_form()
         if form.is_valid():
-            messages.success(self.request,"classroom deleted successfully")
+            messages.success(request, "Classroom deleted successfully")
             return self.form_valid(form)
         else:
-            messages.success(self.request,"Something went wrong")
+            messages.error(request, "Something went wrong !")
             return self.form_invalid(form)
-        
-    
-    
-    
-    
-    
-    
-        
+
+class StudentListView(ListView):
+    model = Student
+    # queryset = Student.objects.all()
+    template_name = "classbased/student.html"
+    context_object_name = "students"
+    success_url = reverse_lazy("classbased:student")
 
 
+class StudentCreateView(CreateView):
+    model = Student
+    template_name = "classbased/student_create.html"
+    form_class = StudentModelForm
+    success_url = reverse_lazy("classbased:student")
+    
+    def form_valid(self, form):
+        # cleaned_data = form.cleaned_data
+        self.object = form.save()
+        student = self.object
+        bio = form.cleaned_data["bio"]
+        phone = form.cleaned_data["phone_number"] 
+        StudentProfile.objects.create(student = student, bio = bio, phone = phone )
+        messages.success(self.request, "Student added successfully")
+        return redirect("classbased:student")
 
 
+    def form_invalid(self, form):
+        messages.error(self.request, "Could not create student!")
+        return super().form_invalid(form)
 
+class StudentDeleteView(DeleteView):
+    pass
+
+class StudentDetailView(DetailView):
+    queryset = Student.objects.all()
+    template_name = "classbased/student_detail.html"
+    context_object_name="student"
